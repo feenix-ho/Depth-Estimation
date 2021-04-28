542927b24e5e6039bc37a89c250a28d3e652760f
Dang Huynh Lam Hai <danghailamhuynh@gmail.com>
2021-04-28 13:22:22 +0700

    Updated train.py

--- rdnet/train.py
+++ rdnet/train.py
@@ -45,42 +45,43 @@ from nystrom_attention import Nystromer
 
 class Arg_train:
     def __init__(self):
-        config = configparser.ConfigParser()
-        config.read('train_arg.txt')
+        configs = configparser.ConfigParser()
+        configs.read('train_arg.txt')
+        config = configs['COLAB']
         # drive/MyDrive/dataset
-        self.data_path = config['DEFAULT']['data_path']
-        self.image_height = int(config['DEFAULT']['image_height'])  # 480
-        self.image_width = int(config['DEFAULT']['image_width'])  # 640
+        self.data_path = config['data_path']
+        self.image_height = int(config['image_height'])  # 480
+        self.image_width = int(config['image_width'])  # 640
         self.image_size = []
         self.image_size.append(self.image_height)
         self.image_size.append(self.image_width)
-        self.patch_size = int(config['DEFAULT']['patch_size'])  # 32
+        self.patch_size = int(config['patch_size'])  # 32
         self.knowledge_dims = list(
-            map(int, config['DEFAULT']['knowledge_dims'].split(',')))  # 4096, 2048, 1024
+            map(int, config['knowledge_dims'].split(',')))  # 4096, 2048, 1024
         self.dense_dims = list(
-            map(int, config['DEFAULT']['dense_dims'].split(',')))  # 1024, 1024, 1024, 1024
-        self.latent_dims = int(config['DEFAULT']['latent_dims'])  # 256
-        self.emb_size = int(config['DEFAULT']['emb_size'])  # 4096
-        self.use_readout = config['DEFAULT']['use_readout']  # ignore
+            map(int, config['dense_dims'].split(',')))  # 1024, 1024, 1024, 1024
+        self.latent_dims = int(config['latent_dims'])  # 256
+        self.emb_size = int(config['emb_size'])  # 4096
+        self.use_readout = config['use_readout']  # ignore
         self.hooks = list(
-            map(int, config['DEFAULT']['hooks'].split(',')))  # 3, 6, 9, 12
-        self.batch_size = int(config['DEFAULT']['batch_size'])  # 4
-        self.num_epochs = int(config['DEFAULT']['num_epochs'])  # 50
-        self.learning_rate = float(config['DEFAULT']['learning_rate'])  # 1e-4
-        self.weight_decay = float(config['DEFAULT']['weight_decay'])  # 1e-2
-        self.adam_eps = float(config['DEFAULT']['adam_eps'])  # 1e-3
-        self.num_threads = int(config['DEFAULT']['num_threads'])  # 1
+            map(int, config['hooks'].split(',')))  # 3, 6, 9, 12
+        self.batch_size = int(config['batch_size'])  # 4
+        self.num_epochs = int(config['num_epochs'])  # 50
+        self.learning_rate = float(config['learning_rate'])  # 1e-4
+        self.weight_decay = float(config['weight_decay'])  # 1e-2
+        self.adam_eps = float(config['adam_eps'])  # 1e-3
+        self.num_threads = int(config['num_threads'])  # 1
         self.data_path_eval = ''
         self.mode = 'train'
         self.checkpoint_path = ''
         self.fix_first_conv_blocks = True
         self.fix_first_conv_block = True
         self.bn_no_track_stats = True
-        self.bts_size = int(config['DEFAULT']['bts_size'])  # 512
+        self.bts_size = int(config['bts_size'])  # 512
         self.retrain = True
         self.end_learning_rate = -1
         self.variance_focus = float(
-            config['DEFAULT']['variance_focus'])  # 0.85
+            config['variance_focus'])  # 0.85
         self.model_name = 'RDnet'
         self.gpu = 0
         self.log_directory = ''
