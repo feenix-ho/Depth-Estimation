54af0513d4845715f4006f2170ab6f69c886e772
Quanggg <quanggr8@gmail.com>
2021-05-01 01:35:27 +0700

    add test.py and update arg_handler

--- rdnet/train.py
+++ rdnet/train.py
@@ -29,7 +29,6 @@ import torch.distributed as dist
 import torch.multiprocessing as mp
 
 from tensorboardX import SummaryWriter
-import configparser
 from nystrom_attention import Nystromer
 
 import matplotlib
@@ -40,55 +39,7 @@ from tqdm import tqdm
 from model import RDNet
 from eval import compute_errors, compute_loss
 from dataloader import *
-
-
-class Arg_train:
-    def __init__(self):
-        configs = configparser.ConfigParser()
-        configs.read('train_arg.txt')
-        config = configs['COLAB']
-        # drive/MyDrive/dataset
-        self.data_path = config['data_path']
-        self.image_height = int(config['image_height'])  # 480
-        self.image_width = int(config['image_width'])  # 640
-        self.image_size = []
-        self.image_size.append(self.image_height)
-        self.image_size.append(self.image_width)
-        self.patch_size = int(config['patch_size'])  # 32
-        self.knowledge_dims = list(
-            map(int, config['knowledge_dims'].split(',')))  # 4096, 2048, 1024
-        self.dense_dims = list(
-            map(int, config['dense_dims'].split(',')))  # 1024, 1024, 1024, 1024
-        self.latent_dims = int(config['latent_dims'])  # 256
-        self.emb_size = int(config['emb_size'])  # 4096
-        self.use_readout = config['use_readout']  # ignore
-        self.hooks = list(
-            map(int, config['hooks'].split(',')))  # 3, 6, 9, 12
-        self.batch_size = int(config['batch_size'])  # 4
-        self.num_epochs = int(config['num_epochs'])  # 50
-        self.learning_rate = float(config['learning_rate'])  # 1e-4
-        self.weight_decay = float(config['weight_decay'])  # 1e-2
-        self.adam_eps = float(config['adam_eps'])  # 1e-3
-        self.num_threads = int(config['num_threads'])  # 1
-        self.data_path_eval = ''
-        self.mode = 'train'
-        self.checkpoint_path = config['checkpoint_path']
-        self.fix_first_conv_blocks = True
-        self.fix_first_conv_block = True
-        self.bn_no_track_stats = True
-        self.bts_size = int(config['bts_size'])  # 512
-        self.retrain = True
-        self.end_learning_rate = -1
-        self.variance_focus = float(
-            config['variance_focus'])  # 0.85
-        self.model_name = 'RDNet'
-        self.gpu = 0
-        self.log_directory = config['log_directory']
-        self.do_online_eval = False
-        self.transformer = Nystromer
-        self.multiprocessing_distributed = False
-        self.log_freq = 100
-        self.save_freq = 500
+from arg_handler import Arg_train
 
 
 DEVICE = torch.device('cuda')
@@ -386,7 +337,6 @@ def main_worker(gpu, ngpus_per_node, args):
             optimizer.zero_grad()
             before_op_time = time.time()
             image = sample_batched['image'].to(DEVICE)
-            focal = sample_batched['focal'].to(DEVICE)
             depth_gt = sample_batched['depth'].to(DEVICE)
             embedding = sample_batched['embedding'].to(DEVICE)
             location = sample_batched['bbox'].to(DEVICE)
