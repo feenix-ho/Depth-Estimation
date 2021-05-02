89d4aae4be37cd80f74f4e14342b8b3be6691406
Dang Huynh Lam Hai <danghailamhuynh@gmail.com>
2021-05-03 00:43:45 +0700

    k

--- rdnet/eval.py
+++ rdnet/eval.py
@@ -62,10 +62,13 @@ def compute_ssi(preds, targets, masks, trimmed=1., eps=1e-4):
 
 def compute_reg(preds, targets, masks, num_scale=4):
     def compute_grad(preds, targets, masks):
-        grads = filters.spatial_gradient(preds - targets)
-        abs_grads = torch.abs(grads[:, :, 0]) + torch.abs(grads[:, :, 1])
-        sum_grads = torch.sum(abs_grads * masks, (2, 3))
-        return sum_grads / masks.sum((2, 3))
+        # grads = filters.spatial_gradient(preds - targets)
+        # abs_grads = torch.abs(grads[:, :, 0]) + torch.abs(grads[:, :, 1])
+        # sum_grads = torch.sum(abs_grads * masks, (2, 3))
+        # return sum_grads / masks.sum((2, 3))
+
+        reg = ((preds - targets) ** 2) * masks
+        return reg.sum((2, 3)) / masks.sum((2, 3))
 
     total = 0
     step = 1
